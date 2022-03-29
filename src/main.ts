726ec4f664c2283aba05e960cd857753de3e19f6
Mike Cavaliere <mike@cavaliere.org>
2022-03-29 16:07:02 -0400

    feat: add CORS whitelisting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ConfigService isn't available until app.listen() is called; we start a temporary server
+  //  to get the CLIENT_HOST from the environment, then close it.
+  await app.listen(3001);
+  const CLIENT_HOST = app.get(ConfigService).get('CLIENT_HOST');
+  await app.close();
+
+  if (!CLIENT_HOST) {
+    throw new Error('CLIENT_HOST is not set.');
+  }
+
+  // Whitelist the client host and start the real server.
+  app.enableCors({ origin: [CLIENT_HOST] });
   await app.listen(3001);
 }
 bootstrap();
